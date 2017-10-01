0d387f16f37a2a245b24a261633a8c3723c20762
charleneangeles <32180011+charleneangeles@users.noreply.github.com>
2017-10-01 10:23:08 -0500

    Angeles iOS Calculator Project

--- iOSCalculator/ViewController.swift
+++ iOSCalculator/ViewController.swift
@@ -11,6 +11,7 @@ enum modes{
     case not_set
     case addition
     case subtraction
+    case multiplication
     
 }
 
@@ -21,6 +22,85 @@ class ViewController: UIViewController {
     var savedNum:Int = 0
     var lastButtonWasMode:Bool = false
 
+    func updateText(){
+        guard let resultsInt:Int = Int(results) else
+        {
+            return
+        }
+        if (currentMode == .not_set){
+            savedNum = resultsInt
+        }
+        // format the output number
+        let formatter:NumberFormatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        let num:NSNumber = NSNumber(value: resultsInt)
+        lblResults.text = formatter.string(from: num)
+        //lblResults.text = "\(num)"
+        
+    }
+    
+    @IBOutlet weak var lblResults: UILabel!
+    func changeMode(newMode:modes){
+        if (savedNum == 0){
+            return
+        }
+        currentMode = newMode
+        lastButtonWasMode = true
+    }
+    @IBAction func btnAdd_Touch(_ sender: Any) {
+        changeMode(newMode:.addition)
+    }
+    
+    @IBAction func btnSubtract_Touch(_ sender: Any) {
+        changeMode(newMode:.subtraction)
+    }
+    
+    @IBAction func btnMultiply_Touch(_ sender: Any) {
+        changeMode(newMode:.multiplication)
+    }
+    
+    @IBAction func btnEqual_Touch(_ sender: Any) {
+        guard let resultsInt:Int = Int(results) else
+        {
+            return
+        }
+        if(lastButtonWasMode){
+            return
+        }
+        // utilize a switch...case statement
+        switch currentMode{
+        case.not_set:
+            return
+        case.addition:
+            savedNum += resultsInt
+        case.subtraction:
+            savedNum -= resultsInt
+        case.multiplication:
+            savedNum *= resultsInt
+        }
+        currentMode = .not_set
+        results = "\(savedNum)"
+        updateText()
+        lastButtonWasMode = true
+    }
+    @IBAction func btnClear_Touch(_ sender: Any) {
+        results = "0"
+        currentMode = .not_set
+        savedNum = 0
+        lastButtonWasMode = false
+        lblResults.text = "0"
+    }
+    
+    @IBAction func btnNumberTouch(_ sender: UIButton) {
+        let selectedNumber:String? = sender.titleLabel?.text// optional
+        if(lastButtonWasMode){
+            lastButtonWasMode = false
+            results = "0"
+        }
+        results = results.appending(selectedNumber!) // unwrap
+        updateText() // call the update text method to show the results in the UI
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
